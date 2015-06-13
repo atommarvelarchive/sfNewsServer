4b19cf2a2a33fd63f2a7629e66e2dc84e25c30f5
Atom Raiff <araiff@yahoo-inc.com>
2015-06-12 17:02:14 -0700

    7x7 has images now

--- story.js
+++ story.js
@@ -19,11 +19,15 @@ module.exports = function(title, url, desc, src, img){
             var $ = cheerio.load(html),
                 og = $('head > meta[property="og:image"]'),
                 twitter = $('head > meta[name="twitter:image:src"]');
-            if(og){
+            if(og.length > 0){
                 self.img = og.attr("content");
-            }else if(twitter){
+            }else if(twitter.length > 0){
                 self.img = twitter.attr("content");
             }else{
+                //console.log(self.url + " does not have a social image");
+                if(self.url.indexOf("7x7.com") !== -1){
+                    self.img = $("#content").find("img").first().attr("src");
+                }
             }
             callback(self);
         });
